d0a7c2ee8a6c971b0a0ddb4917139283b46a3c2f
Shyam Sundar <dssundar@hotmail.com>
2018-04-06 16:23:00 -0700

    initial commit

---
+++ disaster-data/data-cleaner.py
@@ -0,0 +1,20 @@
+
+def load_data(file_path):
+    '''
+    Load the data into a pandas dataframe
+    :param  file_path   path to the dataset
+    :return: pandas dataframe
+    '''
+    pass
+
+def clean_data():
+    '''
+    Clean the data and render the result as a csv
+    :return: None
+    '''
+    pass
+
+
+
+if __name__ == 'main':
+    clean_data()
