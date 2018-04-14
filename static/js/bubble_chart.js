28237ec2137c20f6eabe036150985d64354ab724
Nishanth <reddynishanth16@gmail.com>
2018-04-13 18:10:50 -0700

    final changes

--- static/js/bubble_chart.js
+++ static/js/bubble_chart.js
@@ -1,68 +1,53 @@
 function barChart(containerId, data) {
-    var margin = {top: 20, right: 20, bottom: 70, left: 40},
-        width = 600 - margin.left - margin.right,
-        height = 300 - margin.top - margin.bottom;
+    // set the dimensions and margins of the graph
+    var margin = {top: 20, right: 20, bottom: 30, left: 40},
+        width = 500 - margin.left - margin.right,
+        height = 350 - margin.top - margin.bottom;
 
-// Parse the date / time
-    var	parseDate = d3.time.format("%Y-%m").parse;
+    // set the ranges
+    var x = d3.scaleBand()
+        .range([0, width])
+        .padding(0.1);
+    var y = d3.scaleLinear()
+        .range([height, 0]);
 
-    var x = d3.scale.ordinal().rangeRoundBands([0, width], .05);
-
-    var y = d3.scale.linear().range([height, 0]);
-
-    var xAxis = d3.svg.axis()
-        .scale(x)
-        .orient("bottom")
-        .tickFormat(d3.time.format("%Y-%m"));
-
-    var yAxis = d3.svg.axis()
-        .scale(y)
-        .orient("left")
-        .ticks(10);
-
+    // append the svg object to the body of the page
+    // append a 'group' element to 'svg'
+    // moves the 'group' element to the top left margin
     var svg = d3.select(containerId).append("svg")
-        .attr("width", width + margin.left + margin.right)
+        .attr("width", "100%")
         .attr("height", height + margin.top + margin.bottom)
         .append("g")
         .attr("transform",
             "translate(" + margin.left + "," + margin.top + ")");
 
-        data.forEach(function(d) {
-            d.date = parseDate(d.date);
-            d.value = +d.value;
-        });
+    // format the data
+    data.forEach(function(d) {
+        d[1] = +d[1];
+    });
 
-        x.domain(data.map(function(d) { return d.date; }));
-        y.domain([0, d3.max(data, function(d) { return d.value; })]);
+    // Scale the range of the data in the domains
+    x.domain(data.map(function(d) { return d[0]; }));
+    y.domain([0, d3.max(data, function(d) { return d[1]; })]);
 
-        svg.append("g")
-            .attr("class", "x axis")
-            .attr("transform", "translate(0," + height + ")")
-            .call(xAxis)
-            .selectAll("text")
-            .style("text-anchor", "end")
-            .attr("dx", "-.8em")
-            .attr("dy", "-.55em")
-            .attr("transform", "rotate(-90)" );
+    // append the rectangles for the bar chart
+    svg.selectAll(".bar")
+        .data(data)
+        .enter().append("rect")
+        .attr("class", "bar")
+        .attr("x", function(d) { return x(d[0]); })
+        .attr("width", x.bandwidth())
+        .attr("y", function(d) { return y(d[1]); })
+        .attr("height", function(d) { return height - y(d[1]); });
 
-        svg.append("g")
-            .attr("class", "y axis")
-            .call(yAxis)
-            .append("text")
-            .attr("transform", "rotate(-90)")
-            .attr("y", 6)
-            .attr("dy", ".71em")
-            .style("text-anchor", "end")
-            .text("Value ($)");
+    // add the x Axis
+    svg.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x));
 
-        svg.selectAll("bar")
-            .data(data)
-            .enter().append("rect")
-            .style("fill", "steelblue")
-            .attr("x", function(d) { return x(d.date); })
-            .attr("width", x.rangeBand())
-            .attr("y", function(d) { return y(d.value); })
-            .attr("height", function(d) { return height - y(d.value); });
+    // add the y Axis
+    svg.append("g")
+        .call(d3.axisLeft(y));
 }
 
 function bubbleChart(criteria) {
@@ -151,6 +136,16 @@ function bubbleChart(criteria) {
             })
             .on("mousemove", function() {
                 return tooltip.style("top", (d3.event.pageY - 10) + "px").style("left", (d3.event.pageX + 10) + "px");
+            })
+            .on("click", function(d) {
+                var data = barChartData(d[columnForColors], 'Insured_losses');
+                barChart('#insuredLoss', data);
+
+                data = barChartData(d[columnForColors], 'Total_damage');
+                barChart('#totalDamage', data);
+
+                data = barChartData(d[columnForColors], 'Total_deaths');
+                barChart('#totalLoss', data);
             })
             .on("mouseout", function() {
 
